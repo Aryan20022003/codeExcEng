dc70c471f12fc0f32c8af9fe70f425d0021a2108
aryan20022003 <aryannita20022003@gmail.com>
2024-10-13 11:53:18 +0530

    bug resolve:Worker config update

--- queue/workers.js
+++ queue/workers.js
@@ -1,6 +1,6 @@
-const { worker } = require("bullmq");
-const { path } = require("../src/app");
-const sandboxProcessPath = path.resolve(__dirname, "sandboxProcess.js");
+const { Worker } = require("bullmq");
+const path = require("path");
+const sandboxProcessPath = path.join(__dirname, "sandboxProcess.js");
 const queueName = "globalQueue";
 
 const workers = [];
@@ -9,7 +9,14 @@ const createWorkers = async () => {
   if (workers.length > 0) return workers;
   const workerCount = process.env.WORKER_COUNT || 5;
   for (let i = 0; i < workerCount; i++) {
-    workers.push(new worker(queueName, sandboxProcessPath));
+    workers.push(
+      new Worker(queueName, sandboxProcessPath, {
+        connection: {
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
+        },
+      })
+    );
   }
   console.log("Workers created", Date.now());
   return workers;
